586e55359784c33aa0bd6830e209be78212df43e
BCG814 <bcg814@cs.bham.ac.uk>
2021-11-21 23:13:34 +0000

    Added Xcode project, restructured

---
+++ src/BalanceBot/BalanceBot/BalanceBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalanceBotApp.swift
+//  BalanceBot
+//
+//  Created by Ben Gray on 21/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct BalanceBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ src/BalanceBot/BalanceBot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BalanceBot
+//
+//  Created by Ben Gray on 21/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
